b3726241bd9684b77556c95d3ea32e93d2197662
Sihan Tawsik <sihantawsik@gmail.com>
2023-05-01 14:27:43 +0600

    Add pipelines

--- .cicada/ts-cicd.ts
+++ .cicada/ts-cicd.ts
@@ -1,20 +1,20 @@
 import { Job, Pipeline } from "https://deno.land/x/cicada@v0.1.50/mod.ts";
 
 const build = new Job({
-  name: "Node Build",
-  image: "node",
-  steps: [
-    {
-      name: "Install Dependencies",
-      run: "npm install",
-      cacheDirectories: ["node_modules"],
-    },
-    {
-      name: "Run build",
-      run: "npm run build",
-      cacheDirectories: ["node_modules"],
-    },
-  ],
+    name: "Node Build",
+    image: "node",
+    steps: [
+        {
+            name: "Install Dependencies",
+            run: "yarn install",
+            cacheDirectories: ["node_modules"],
+        },
+        {
+            name: "Run build",
+            run: "yarn build",
+            cacheDirectories: ["node_modules"],
+        },
+    ],
 });
 
 export default new Pipeline(
